9f39ab1cfb90cf557ccbfeea1656d65adbb65752
Bin Zhao <zhaobin@oneapm.com>
2015-12-09 19:24:34 +0800

    add chapter one

---
+++ chapter_1/1_1.lua
@@ -0,0 +1,14 @@
+-- defines a factorial function
+function fact (n)
+    if n < 0 then
+        error("Invalid number")
+    elseif n == 0 then
+        return 1
+    else
+        return n * fact(n - 1)
+    end
+end
+
+print("enter a number:")
+a = io.read("*n")
+print(fact(a))
